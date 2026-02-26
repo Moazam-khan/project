83dd9258173d0313b4785933c62af9b33156fbd9
Engr Moazam khan <moazamkhan8999@gmail.com>
2026-02-26 14:53:17 +0300

    passing data to views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,6 @@
 <x-layout >
-    <h1>Welcome to My Website without layout</h1>
-    <p>This is the home page of my Laravel application.</p>
+    <!-- <h1>Welcome to My Website without layout</h1>
+    <p>This is the home page of my Laravel application.</p> -->
+
+    {{ $greeting}} {{ $name}}
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');      // Home page
+Route::view('/', 'welcome', [
+    'greeting' => 'Welcome',
+    'name' => request ('name' ,' Prince' ) //query string
+]);  
+// Route::view('/', 'welcome');      // Home page
 Route::view('/about', 'about');   // About page
 Route::view('/contact', 'contact'); // Contact page
+
+   // Home page
